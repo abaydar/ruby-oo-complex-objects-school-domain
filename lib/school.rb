63c08f3462d2cebe61e0821ba67e4c1285cefd85
+ <+>
2020-11-24 16:21:42 -0500

    commit tree e10bc422811447fa386daaaef52ae02d8d370fa1

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,33 @@
 # code here!
+class School
+
+
+    def initialize(school_name)
+        @school_name = school_name
+        @roster = Hash.new
+    end
+    
+    def roster
+        @roster
+    end
+
+    def add_student(name, grade)
+        @name = name
+        @grade = grade
+        (@roster[grade] ||= []) << name
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        sorted_roster = {}
+        @roster.map do |grade, names|
+            sorted_roster[grade] = names.sort
+        end
+        sorted_roster
+    end
+
+
+end
